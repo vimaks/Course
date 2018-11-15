711ae220abac34e0c22b714c3cbe596f56f305d7
Maksym Vinichenko <vinimaks140@gmail.com>
2018-11-15 15:16:40 +0200

    Factorial

--- Homeworks/Homework_09112018_Vinichenko_Maksym/Lab Work 2.2 (program code 2)_Hello_fact_advstud/Hello_fact_advstud/Hello_fact_advstud/Program.cs
+++ Homeworks/Homework_09112018_Vinichenko_Maksym/Lab Work 2.2 (program code 2)_Hello_fact_advstud/Hello_fact_advstud/Hello_fact_advstud/Program.cs
@@ -11,14 +11,29 @@ namespace Hello_fact_advstud
         static void Main(string[] args)
         {
             //Define parameters to calculate the factorial of
+            Console.WriteLine($"Enter number for facrorial ");
+            int number;
+            string input = Console.ReadLine();
+            while (int.TryParse(input, out number) == false)
+            {
+                Console.WriteLine($"Enter int number for facrorial ");
+                input = Console.ReadLine();
+            }
+            number = int.Parse(input);
+
             //Call fact() method to calculate
+            Console.WriteLine($"{number}! = {Fact(number)}");
+            Console.ReadLine();
+
         }
 
         //Create fact() method  with parameter to calculate factorial
         //Use ternary operator
-
+        public static int Fact(int i)
+        {
+            i = i <= 1 ? 1 : i * Fact(i - 1);
+            return i;
+        }
     }
-
-    
-
 }
+
