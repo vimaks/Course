4cc12eab5cb9fefb32c12562a7a994aca0a19def
Maksym Vinichenko <vinimaks140@gmail.com>
2018-11-18 13:12:00 +0200

    Lab 2.3 done

--- Homeworks/Homework_14112018_Vinichenko_Maksym/Lab_2.3/CSharp_Net_module1_2_3_lab/CSharp_Net_module1_2_3_lab/Money.cs
+++ Homeworks/Homework_14112018_Vinichenko_Maksym/Lab_2.3/CSharp_Net_module1_2_3_lab/CSharp_Net_module1_2_3_lab/Money.cs
@@ -7,26 +7,99 @@ using System.Threading.Tasks;
 namespace CSharp_Net_module1_2_3_lab
 {
     // 1) declare enumeration CurrencyTypes, values UAH, USD, EU
+    public enum CurrencyTypes
+    {
+        UAH,
+        USD,
+        EU
+    }
 
-
-    class Money
+    public class Money
     {
         // 2) declare 2 properties Amount, CurrencyType
-
+        public decimal Amount { get; set; }
+        public CurrencyTypes Types { get; set; }
         // 3) declare parameter constructor for properties initialization
+        public Money(decimal amount, CurrencyTypes types )
+        {
+            Amount = amount;
+            Types = types;
+        }
 
         // 4) declare overloading of operator + to add 2 objects of Money
+        public static Money operator +(Money money1, Money money2)
+        {
+            if (money1.Types == money2.Types)
+            {
+                return new Money(money1.Amount + money2.Amount, money1.Types);
+            }else
+            return null;
+        }
 
         // 5) declare overloading of operator -- to decrease object of Money by 1
-
+        public static Money operator --(Money money1)
+        {
+            money1.Amount = money1.Amount - 1;
+            return money1;
+        }
         // 6) declare overloading of operator * to increase object of Money 3 times
-
+        public static Money operator *(Money money1, Money money2)
+        {
+            money1.Amount = money1.Amount * 3;
+            return money1;
+        }
         // 7) declare overloading of operator > and < to compare 2 objects of Money
+        public static bool operator >(Money money1, Money money2)
+        {
+            if (money1.Amount > money2.Amount)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator <(Money money1, Money money2)
+        {
+            if (money1.Amount < money2.Amount)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
         // 8) declare overloading of operator true and false to check CurrencyType of object
+        
+        public static bool operator true(Money money1)
+        {
+            Console.WriteLine(money1.Types);
+            return true;
+        }
 
+        public static bool operator false(Money money1)
+        {
+
+            Console.WriteLine(money1.Types);
+            return false;
+        }
+        
         // 9) declare overloading of implicit/ explicit conversion  to convert Money to double, string and vice versa
+        public static explicit operator double(Money money)
+        {
+            return Convert.ToDouble(money.Amount);
+        }
+
+        public static explicit operator string(Money money)
+        {
+            return money.Amount.ToString();
+        }
 
 
-    }
+
+
+        }
 }
--- Homeworks/Homework_14112018_Vinichenko_Maksym/Lab_2.3/CSharp_Net_module1_2_3_lab/CSharp_Net_module1_2_3_lab/Program.cs
+++ Homeworks/Homework_14112018_Vinichenko_Maksym/Lab_2.3/CSharp_Net_module1_2_3_lab/CSharp_Net_module1_2_3_lab/Program.cs
@@ -11,23 +11,45 @@ namespace CSharp_Net_module1_2_3_lab
         static void Main(string[] args)
         {
             // 10) declare 2 objects
-
+            Money money1 = new Money(100, CurrencyTypes.UAH);
+            Money money2 = new Money(200, CurrencyTypes.UAH);
             // 11) do operations
             // add 2 objects of Money
+            Money money3 = money1 + money2;
+            Console.WriteLine($"{money3.Amount}  {money3.Types}");
 
             // add 1st object of Money and double
+            money1.Amount = money1.Amount + (decimal)140.4d;
 
             // decrease 2nd object of Money by 1 
+            money2--;
 
             // increase 1st object of Money
+            money1 = money1 * money1;
 
             // compare 2 objects of Money
 
+            bool b = money1 > money2;
+
+
             // compare 2nd object of Money and string
-
+            // не понял задание
             // check CurrencyType of every object
+            if (money1)
+            {
 
+            }
+            if (money2)
+            {
+
+            }
+            if (money3)
+            {
+
+            }
             // convert 1st object of Money to string
+            string s = (string)money1;
+            Console.ReadLine();
 
         }
     }
