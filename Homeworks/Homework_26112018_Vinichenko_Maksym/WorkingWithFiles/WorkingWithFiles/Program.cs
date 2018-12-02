9d6ea549210a410c479e61a758a28dfb6e9d7d08
Dmytro Kulak <dmytro.kulak@ssa-outsourcing.com>
2018-12-02 13:51:54 +0200

    [update] work with files

--- Homeworks/Homework_26112018_Vinichenko_Maksym/WorkingWithFiles/WorkingWithFiles/Program.cs
+++ Homeworks/Homework_26112018_Vinichenko_Maksym/WorkingWithFiles/WorkingWithFiles/Program.cs
@@ -6,15 +6,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace WorkingWithFiles
-{
+{    
     class Program
     {
         static void Main(string[] args)
         {
-
+            
             DirectoryInfo directory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
             directory.Parent.CreateSubdirectory("SomeDir");
             string path = directory.Parent.FullName + "\\SomeDir\\";
+            //Помним что есть Path.Combine(), как то Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SomeDir")
             string tempText = "";
             using (var file1 = File.Create(path + "text1.txt"))
             {
@@ -23,16 +24,20 @@ namespace WorkingWithFiles
 
             using (Stream s = new FileStream(path + "text1.txt", FileMode.Open, FileAccess.ReadWrite))
             {
-                string text = "Jackdaws love my big sphinx of quartz.";
-                byte[] arrBytes = text.SelectMany(c => BitConverter.GetBytes(c)).ToArray();
+                string text = "Jackdaws love my big sphinx of quartz.";                
+                byte[] arrBytes = text.SelectMany(c => BitConverter.GetBytes(c)).Where(a => a != 0).ToArray();
+                //Если у Вас появились нули то самое быстрое костыльное решение - их отфильтровать на этапе преобразования. Проблема в кодировке
+                //Потому как если получить байты таким образом var smt = Encoding.ASCII.GetBytes(text.ToCharArray()); то лишних нулей уже нет     
+                //А так Encoding.UTF32.GetBytes(text.ToCharArray()) нулей будет еще больше
+                //видимо у Вас сам файлик Program.cs сохранен в UTF16 или чем-то подобном
                 s.Write(arrBytes, 0, arrBytes.Length);
                 s.Flush();
             }
 
             using (StreamReader sr = new StreamReader(path + "text1.txt"))
-            {
-                tempText = sr.ReadToEnd();
-                tempText = tempText.Replace("\0", String.Empty);
+            {                
+                string text = sr.ReadToEnd();                
+                //tempText = tempText.Replace("\0", String.Empty);
             }
 
             using (StreamWriter sw = new StreamWriter(path + "text2.txt"))
